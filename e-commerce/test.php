4880239603fecfa24d379b661dcb8e0532125144
paldenkhamba <56550239+paldenkhamba@users.noreply.github.com>
2021-09-10 20:06:24 +0545

    Delete test.php

--- e-commerce/test.php
+++
@@ -1,8 +0,0 @@
-<div class="header"><ul class="float-left"><li>Connect with us</li><li><img src="admin\img\fb.png"></li><li><img src="admin\img\insta.png"></li><li><img src="admin\img\twitter.png"></li><li>contact us +97715970000</li></ul>
-        <ul class="float-right"><li><?php if(isset($_SESSION['first_name']))
-echo $_SESSION['first_name']."'s account";?>
-</li><li>| SELL ON Omelete  |</li><LI>HELP CENTER   |</LI><LI>TRACK ORDER</LI></ul>
-    </div>
-       
-    <hr style="color: lightgray;">
-        
